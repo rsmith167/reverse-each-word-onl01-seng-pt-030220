14cb56ad49f48cc60e8cc37181a64b09259c3f37
Rwanda S <rwanda.ace@gmail.com>
2020-03-09 18:27:32 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,10 @@
 def reverse_each_word(string)
-  # word_string = []
-  # word_array = string.split(" ")
-  # word_array.each do |word|
-  #   word_string << word.reverse
-  # end
-  # word_string.join(" ")
+  word_string = []
+  word_array = string.split(" ")
+  word_array.each do |word|
+    word_string << word.reverse
+  end
+  word_string.join(" ")
   
   word_array = string.split(" ")
   word_array.collect do |word|
